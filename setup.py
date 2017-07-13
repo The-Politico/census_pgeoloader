0fd4ccc89e2ec853388653c86b0ca9c1b2736860
Jon McClure <jmcclure@politico.com>
2017-07-13 17:41:55 -0400

    0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="census_pgeoloader",
-    description="Load Census shapefiles into PostGIS"
+    description="Load Census shapefiles into PostGIS",
     long_description=README,
     url=REPO_URL,
     version='0.1.1',
